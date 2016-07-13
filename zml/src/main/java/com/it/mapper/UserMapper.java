38a26217637f06fc57fae7c0b8adb8c6f0901196
anze-21 <zml_anze_92@163.com>
2016-07-13 17:39:24 +0800

    完成用户列表的显示

--- zml/src/main/java/com/it/mapper/UserMapper.java
+++ zml/src/main/java/com/it/mapper/UserMapper.java
@@ -1,8 +1,18 @@
 package com.it.mapper;
 import com.it.pojo.User;
+
+import java.util.List;
+import java.util.Map;
+
 public interface UserMapper {
 
     User findByUsername(String username);
 
     void updateUser(User user);
+
+    List<User> findByParam(Map<String, Object> params);
+
+    Long count();
+
+    Long countByParam(Map<String, Object> params);
 }
---
+++ zml/src/main/java/com/it/util/Strings.java
@@ -0,0 +1,18 @@
+package com.it.util;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+
+public class Strings {
+    public static String toUTF8(String str) {
+        if (StringUtils.isNotEmpty(str)) {
+            try {
+                return new String(str.getBytes("ISO8859-1"), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("字符串转码异常");
+            }
+        }
+        return "";
+    }
+}
