84aac00d3b034a82ae8a0d061180eb50af9546c4
anze-21 <zml_anze_92@163.com>
2016-07-18 13:36:57 +0800

    完成『销售机会』的静态页面

--- zml/src/test/java/com/it/Test.java
+++ zml/src/test/java/com/it/Test.java
@@ -13,6 +13,7 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -21,7 +22,7 @@ import java.util.Hashtable;
 
 public class Test {
     public static void main(String[] args) throws BadHanyuPinyinOutputFormatCombination, WriterException, IOException {
-        // System.out.println(DigestUtils.md5Hex("000000"));
+         System.out.println(DigestUtils.md5Hex("123123"));
 //        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
 //        format.setCaseType(HanyuPinyinCaseType.LOWERCASE);//让拼音小写
 //        format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);//不要音调
@@ -32,11 +33,11 @@ public class Test {
        // BitMatrix bitMatrix =new MultiFormatWriter().encode("Hello", BarcodeFormat.QR_CODE,
       //  200,200);
        // String mecard = "MECARD:N:樊凯;ORG:凯盛软件;TEL:15138041672;EMAIL:fankai@kaishengit.com;ADR:中国;;";
-        String mecard = "哈哈，没问题,我们会把你吃穷的额~~~;";
-        Hashtable hints =new Hashtable();
-        hints.put(EncodeHintType.CHARACTER_SET,"UTF-8");
-        BitMatrix bitMatrix =new MultiFormatWriter().encode(mecard,BarcodeFormat.QR_CODE,400,400,hints);
-        MatrixToImageWriter.writeToStream(bitMatrix,"png",new FileOutputStream("D:/qr.png"));
+//        String mecard = "哈哈，没问题,我们会把你吃穷的额~~~;";
+//        Hashtable hints =new Hashtable();
+//        hints.put(EncodeHintType.CHARACTER_SET,"UTF-8");
+//        BitMatrix bitMatrix =new MultiFormatWriter().encode(mecard,BarcodeFormat.QR_CODE,400,400,hints);
+//        MatrixToImageWriter.writeToStream(bitMatrix,"png",new FileOutputStream("D:/qr.png"));
 
 
 
